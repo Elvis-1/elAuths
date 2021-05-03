099c9f653adf9f9f7c1b733179df8cd62f7414f4
Elvis <e.igiebor@cyberticks.com>
2021-05-03 05:28:03 +0100

    auth website

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.app')
+ @section('content')
+ 
+
+<div class="container">
+<div class="card">
+  <div class="card-header" style="text-align:center">
+    <h1>First Lauched Laravel App</h1>
+  </div>
+  <div class="card-body">
+    <h5 class="card-title">More apps on the way</h5>
+    <p class="card-text">Thank God for the success of this first app.Pls explore, use the registration link in the header then login. Thanks</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+</div>
+ @endsection
